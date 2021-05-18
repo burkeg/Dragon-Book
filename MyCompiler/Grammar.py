30d58d3e5fe71699c2b44d1346020b077c262f57
BuildTools <burkeg@purdue.edu>
2021-05-18 00:08:56 -0700

    Recognizing the rules for basic grammars. Next step is to maybe work on a lexer or syntax analyzer?

--- MyCompiler/Grammar.py
+++ MyCompiler/Grammar.py
@@ -1,10 +1,26 @@
-class Terminal:
+import re
+
+class Symbol:
     def __init__(self, string):
         self.string = string
 
-class Nonterminal:
-    def __init__(self, name):
-        self.name = name
+    def __hash__(self):
+        return hash(self.string)
+
+    def __eq__(self, other):
+        if not isinstance(other, Symbol):
+            return False
+        return hash(self) == hash(other)
+
+    def __str__(self):
+        return self.string
+    __repr__ = __str__
+
+class Terminal(Symbol):
+    pass
+
+class Nonterminal(Symbol):
+    pass
 
 class Grammar:
     def __init__(self, terminals, nonterminals, productions, start_symbol):
@@ -13,5 +29,69 @@ class Grammar:
         self.productions = productions      # A dictionary of nonterminals to lists of terminals/nonterminals
         self.start_symbol = start_symbol    # The starting point for all derivations from this grammar
 
-    def from_string(self, grammar_as_string):
-        pass
+    @staticmethod
+    def from_string(grammar_as_string):
+        # Nonterminals are strings of \w.
+        # Terminals are strings enclosed in quotes.
+        # A production is a Nonterminal followed by -> and a list of terminals/nonterminals.
+        # As shorthand multiple productions can be separated by a | on the right hand side.
+        # A single production rule must be contained on 1 line.
+        # All terminals and non terminals are separated by whitespace.
+        # Start symbol is the first nonterminal defined.
+        start_symbol = None
+        productions = dict()
+        current_nonterminal = None
+        for line in grammar_as_string.split('\n'):
+            rest_of_line = line.strip()
+            if m := re.match(r'^(\w+) -> (.*)$', rest_of_line):
+                # This is a definition of a nonterminal, let's figure out its name
+                current_nonterminal = Nonterminal(m.group(1))
+                rest_of_line = m.group(2).strip()
+                if start_symbol is None:
+                    start_symbol = current_nonterminal
+            elif m := re.match(r'^\|(.*)$', rest_of_line):
+                # we are on a new line doing a shorthand production
+                current_rule = []
+                rest_of_line = m.group(1).strip()
+
+            current_rule = []
+            # Let's keep grabbing productions rules from the string until none remain
+            while len(rest_of_line) > 0:
+                if m := re.match(r'^(\w+)(.*)$', rest_of_line):
+                    # Found nonterminal
+                    current_rule.append(Nonterminal(m.group(1)))
+                    rest_of_line = m.group(2).strip()
+                elif m := re.match(r'^\"([^\"]+?)\"(.*)$', rest_of_line):
+                    # Found terminal
+                    current_rule.append(Terminal(m.group(1)))
+                    rest_of_line = m.group(2).strip()
+                elif m := re.match(r'^\|(.*)$', rest_of_line):
+                    # shorthand for starting up another production on this line
+                    productions.setdefault(current_nonterminal, []).append(current_rule)
+                    current_rule = []
+                    rest_of_line = m.group(1).strip()
+                else:
+                    raise Exception('Expected a nonterminal, terminal or | before "' + rest_of_line + '"')
+
+            productions.setdefault(current_nonterminal, []).append(current_rule)
+        nonterminals = set(productions.keys())
+        all_rhs_symbols = set([symbol for prod_list in productions.values() for production in prod_list for symbol in production])
+        terminals = all_rhs_symbols.difference(nonterminals)
+        return Grammar(terminals, nonterminals, productions, start_symbol)
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    with open('Grammars/grammar4.1.txt', 'r') as file:
+        g = Grammar.from_string(file.read())
+    print('terminals: ', g.terminals)
+    print('nonterminals: ', g.nonterminals)
+    print('productions: ', g.productions)
+    print('start_symbol: ', g.start_symbol)
+
+
+
